2709c8887bfa88513e32b9805167175bdfb3bf15
albertdwag <albertcesar463@gmail.com>
2023-03-20 20:00:17 -0300

    adding scripts.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
 
-    // Update is called once per frame
+    public string tagToCheckEnemy = "Enemy";
+    public string tagToCheckEndLine = "EndLine";
+    public float speed = 1f;
+
+    private bool _canRun;
+    private Vector3 _pos;
+
+    public GameObject endScreen;
+
     void Update()
     {
-        transform.Translate(transform.forward * speed * Time.deltaTime);   
+        if (!_canRun) return;
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == tagToCheckEndLine)
+            EndGame();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == tagToCheckEndLine)
+            EndGame();
+    }
+
+    private void EndGame()
+    {
+        _canRun = false;
+        endScreen.SetActive(true);
+    }
+
+    public void StartToRun()
+    {
+        _canRun = true;
     }
 }
