43c61fbd14b9cdd4580a7d70e853a34eaadd7734
albertdwag <albertcesar463@gmail.com>
2023-03-20 18:45:59 -0300

    adding coins and player movement.

---
+++ Assets/Scripts/Items/ItemCollactableBase.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollactableBase : MonoBehaviour
+{
+    public string compareTag = "Player";
+    public ParticleSystem particles;
+    public float timeToHide = 3f;
+    public GameObject graphicItem;
+
+    [Header("Sounds")]
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        //if (particles != null) particles.transform.SetParent(null);
+    }
+
+    protected void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.CompareTag(compareTag))
+            Collect();
+
+        gameObject.GetComponent<Collider>().enabled = false;
+    }
+
+    protected virtual void Collect()
+    {
+        if (graphicItem != null) graphicItem.SetActive(false);
+        Invoke("HideObject", timeToHide);
+        OnCollect();
+    }
+
+    private void HideObject()
+    {
+        gameObject.SetActive(false);
+    }
+
+    protected virtual void OnCollect()
+    {
+        if (particles != null) particles.Play();
+        if (audioSource != null) audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/Items/ItemManager.cs
@@ -0,0 +1,24 @@
+using Assets.Core.Singleton;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : Singleton<ItemManager>
+{
+    public SOInt coins;
+
+    private void Start()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        coins.value = 0;
+    }
+
+    public void AddCoins(int amount = 1)
+    {
+        coins.value += amount;
+    }
+}
